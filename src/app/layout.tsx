59691d45b09bcfff06c031220a95c655f0e32bf2
Joao Senco <joao@senco.dev.br>
2024-10-22 17:18:09 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Sidebar from "../components/sidebar/index"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
   weight: "100 900",
 });
 
@@ -26,8 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          geistSans.className
+        )}
+      >  
+        <Sidebar/>
         {children}
       </body>
     </html>
